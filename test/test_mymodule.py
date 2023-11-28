13930397a730e2e55c3cbd20b4f68da9b2dba390
vikasharitwal93 <vikas.haritwal@harness.io>
2023-11-28 12:05:23 +0530

    Test add

---
+++ test/test_mymodule.py
@@ -0,0 +1,12 @@
+import unittest
+from mymodule import add_numbers
+
+class TestMyModule(unittest.TestCase):
+
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+        self.assertEqual(add_numbers(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
